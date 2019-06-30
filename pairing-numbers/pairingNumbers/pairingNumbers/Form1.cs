b028e9d5b850deeb3ee87231dbab9660d5a1e88d
En Emerson <007emersone@gmail.com>
2019-06-29 20:19:30 -0500

    write sum check logic

--- pairing-numbers/pairingNumbers/pairingNumbers/Form1.cs
+++ pairing-numbers/pairingNumbers/pairingNumbers/Form1.cs
@@ -14,7 +14,6 @@ namespace pairingNumbers
     {
         // will contain the sorted list of ints
         int[] numbers = new int[10];
-
         // inclusive
         private static int MIN_VAL = 1;
         // exclusive
@@ -27,14 +26,14 @@ namespace pairingNumbers
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // clear out any old numbers that may remain
+            numsBox.Items.Clear();
             // create a new random generator
             Random rand = new Random();
-            // create 10 new random ints between the max and min values
+            // fill array with 10 new random ints between the max and min values
             for(int x = 0; x < numbers.Length; x++)
             {
-                int nextInt = rand.Next(MIN_VAL, MAX_VAL);
-                numbers[x] = nextInt;
-
+                numbers[x] = rand.Next(MIN_VAL, MAX_VAL);
             }
 
             // sort the array of ints, ascending
@@ -48,11 +47,49 @@ namespace pairingNumbers
 
         private void button2_Click(object sender, EventArgs e)
         {
-            /*
+            /* Algorithm:
              * 1. Iterate through the array
              * 2. Subtract current value from the target number
              * 3. Check if remainder exists in array
              */
+
+            // check if target number data is valid
+            int targetNumber = 0;
+            bool goodData = Int32.TryParse(testBox.Text, out targetNumber);
+            if (goodData && (MIN_VAL <= targetNumber))
+            {
+                // set target number from the test box input
+                targetNumber = int.Parse(testBox.Text);
+                // 1. begin iteration thru numbers array
+                for (int num = 0; num < numbers.Length; num++)
+                {
+                    int currentNumber = numbers[num];
+                    // 2. subtract current value from the target number
+                    int remainder = targetNumber - currentNumber;
+                    // 3a. iterate through rest of array
+                    for (int checkNum = num + 1; checkNum < numbers.Length; checkNum++)
+                    {
+                        int nextNumber = numbers[checkNum];
+                        // 3b. check if remainder is in the array
+                        if (remainder == nextNumber)
+                        {
+                            // found a match!
+                            MessageBox.Show("Match found! \r\n'"
+                                + currentNumber + "' at index: [" + num + "]"
+                                + "\r\n AND \r\n'"
+                                + nextNumber + "' at index: [" + checkNum + "]\r\n"
+                                + " add up to the target number: '" + targetNumber + "'");
+                            Application.Exit();
+                        }
+                    }
+                }
+                // none of the numbers match
+                MessageBox.Show("No two integers in this list add up to the target number.");
+            }
+            else
+            {
+                MessageBox.Show("Bad input data");
+            }
         }
     }
 }
